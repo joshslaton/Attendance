d413c262cd4dff05d8166502c31cdf5010ed80e7
joshslaton <alecjoshua.slaton@lorma.edu>
2019-05-06 22:28:19 +0800

    clean

--- reports/attendance/section/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Calculate school days
-  // Y-m-D
-  $startDate = "2018-08-13";
-  $endDate = "2019-05-31";
-  $datediff = strtotime($endDate) - strtotime($startDate);
-  print_r($datediff / (60 * 60 * 24));
-?>
